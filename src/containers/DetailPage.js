bfbcaab5b30898d4701c0b16d563f50d53f301b6
Vandhuy <vandhuy@gmail.com>
2018-02-01 01:24:15 -0300

    #5 Create Detail Page

--- src/containers/DetailPage.js
+++ src/containers/DetailPage.js
@@ -4,22 +4,52 @@ import { withRouter } from 'react-router-dom'
 import moment from 'moment';
 import styles from '../styles/DetailPage.scss';
 
+const MAX_VIDEOS_RESULTS = 30;
+const NUMBER_OF_VIDEOS_TO_LOAD = 5;
+
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+  }
+
+  componentDidMount() {
+    this.renderChunkOfVideos();
+  }
+
+  renderChunkOfVideos() {
+    const loadedVideos = this.props.currentSearch.videos;
+    const resultsLength = loadedVideos.length;
+    const videosRenderedLength = this.state.videos.length;
+
+    if (videosRenderedLength < resultsLength) {
+      const chunk = loadedVideos.slice(videosRenderedLength, videosRenderedLength + NUMBER_OF_VIDEOS_TO_LOAD);
+
+      this.setState({
+        videos: this.state.videos.concat(chunk)
+      })
+    }
+  }
+
+  facebookUrlFilter(url) {
+    if (url.indexOf('pages') >= 0) {
+      return '@'.concat(
+        url.replace('http://www.facebook.com/pages/', '')
+          .replace(/%20/g, '')
+          .split('/')[0]
+      );
+    } else {
+      return '@'.concat(
+        url.replace('https://www.facebook.com/', '').replace('/', '')
+      );
+    }
+  }
+
   render() {
     const { history, currentSearch } = this.props;
-
-    const facebookUrl = (url) => {
-      if (url.indexOf('pages') >= 0)
-        return '@'.concat(
-          url.replace('http://www.facebook.com/pages/', '')
-            .replace(/%20/g, '')
-            .split('/')[0]
-        );
-      else
-        return '@'.concat(
-          url.replace('https://www.facebook.com/', '').replace('/', '')
-        );
-    };
 
     return (
       <div className="detail-page">
@@ -41,7 +71,7 @@ class HomePage extends Component {
                   return <span ng-if="vm.artist.details.facebook_page_url == ''" className="url">No facebok profile.</span>;
                 } else {
                   return (<a target="_blank" href={ currentSearch.details.facebook_page_url } className="url">
-                    { facebookUrl(currentSearch.details.facebook_page_url) }</a>);
+                    { this.facebookUrlFilter(currentSearch.details.facebook_page_url) }</a>);
                 }
               })()}
               </div>
@@ -75,9 +105,31 @@ class HomePage extends Component {
             </div>
           </div>
         </div>
-
-
-
+        <hr className="visible-xs-block"/>
+        <div className="artist-videos">
+          <h4>RELATED VIDEOS</h4>
+          {this.state.videos.map(video => {
+            return(
+              <div key={video.id.videoId} className="video">
+                <div className="thumb">
+                  <a target="_blank" href={`https://www.youtube.com/watch?v=${video.id.videoId}`}>
+                    <img src={ video.snippet.thumbnails.medium.url } alt="Youtube Video Thumbnail"/>
+                  </a>
+                </div>
+                <div className="name">
+                  <a target="_blank" className="title" href={`https://www.youtube.com/watch?v=${ video.id.videoId }`}>
+                    { video.snippet.title }
+                  </a>
+                  <span className="channel">{ video.snippet.channelTitle }</span>
+                </div>
+              </div>
+            );
+          })}
+          { (this.state.videos.length !== MAX_VIDEOS_RESULTS) ?
+            <button className="btn bnt-lg btn-default btn-show-more"
+                    onClick={() => this.renderChunkOfVideos()}>Show More</button> : ''
+          }
+        </div>
       </div>
     );
   }
